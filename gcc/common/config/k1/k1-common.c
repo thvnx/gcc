c3889e862f072548b3d2b067a1536e7c7abcc30d
Marc Poulhiès <marc.poulhies@kalray.eu>
2017-04-05 11:16:46 +0200

    [TO REBASE] add back -march for compat

--- gcc/common/config/k1/k1-common.c
+++ gcc/common/config/k1/k1-common.c
@@ -57,7 +57,7 @@ k1_handle_option (struct gcc_options *opts ATTRIBUTE_UNUSED,
     case OPT_ffixed_reg:  /* defer this option */
     case OPT_mcore_k1pe:
     case OPT_mcore_k1rm:
-    /* case OPT_march_: */
+    case OPT_march_:
     /* case OPT_msoc_: */
     case OPT_mboard_:
     case OPT_mcluster_:
