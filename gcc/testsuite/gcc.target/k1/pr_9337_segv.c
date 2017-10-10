006127c5d76effd554ff5141709b13a7f5026112
Marc Poulhiès <marc.poulhies@kalray.eu>
2017-10-10 10:19:34 +0200

    [K1] disable waitclr test on coolidge
         * gcc.target/k1/pr_9337_segv.c: Disable on anything but
         Bostan. Should be completely deleted as Bostan is not supported
         anymore.

--- gcc/testsuite/gcc.target/k1/pr_9337_segv.c
+++ gcc/testsuite/gcc.target/k1/pr_9337_segv.c
@@ -1,5 +1,6 @@
 /* { dg-do  compile } */
 /* { dg-options "-O2"  } */
+/* { dg-skip-if "Only supported on Bostan" { *-*-* }  { "*" } { "-march=k1b" } } */
 
 void exec_control_engine( void ) {
   while(1)
