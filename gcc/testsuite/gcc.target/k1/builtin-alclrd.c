42c1c393a744393f2902f870ddd0567434217aef
Marc Poulhiès <marc.poulhies@kalray.eu>
2018-07-13 10:34:39 +0200

    [K1] fix bad regex in tests
    	* gcc/testsuite/gcc.target/k1/builtin-alclrd.c: Fix regexp.
    	* gcc/testsuite/gcc.target/k1/builtin-alclrw.c: Likewise.

--- gcc/testsuite/gcc.target/k1/builtin-alclrd.c
+++ gcc/testsuite/gcc.target/k1/builtin-alclrd.c
@@ -1,6 +1,6 @@
 /* { dg-do compile } */
 /* { dg-options "-O2" } */
-/* { dg-final { scan-assembler "alclrd \\\$r\[0-9\]+ = 0\\\[$r\[0-9\]+\\\]" } } */
+/* { dg-final { scan-assembler "alclrd \\\$r\[0-9\]+ = 0\\\[\\\$r\[0-9\]+\\\]" } } */
 /* { dg-skip-if "Test valid only on Coolidge" { *-*-* }  {  "*" } { "-march=k1c" } } */
 
 unsigned int f(void *addr) {
