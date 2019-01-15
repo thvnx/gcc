89073204bdfb3d198898c013f6191d70777994b7
Marc Poulhiès <marc.poulhies@kalray.eu>
2019-01-15 12:00:53 +0100

    Try to fix hardware loop
    The loop_end pattern being a jump insn and having output operands
    can't be reloaded correctly by the reload pass. A previous change was
    introduced, inspired by arc's backend, but it was not correctly
    implemented. When reload needs to reload the loop counter, it gives a
    MEM rtx that is temporaly accepted by the pattern. This memory
    location still needs to be stored in a register and wrote back when
    finished: this is what this change does.
    
    ref T7970
    
    	gcc/
    	* config/k1/k1.c (hwloop_fail): Detect case of loop_end that needs
    	to be reloaded and emit corresponding mem write.
    	(hwloop_pattern_reg): Match pattern with reload.
    	* config/k1/k1.md (doloop_end): Add a scratch register used when
    	reload needs to be handled.
    	(loop_end): Convert to split. Split in case reload is needed.
    	(loop_end_withreload): New.

--- gcc/config/k1/k1.c
+++ gcc/config/k1/k1.c
@@ -8191,6 +8191,7 @@ hwloop_fail (hwloop_info loop)
 {
   rtx test;
   rtx insn = loop->loop_end;
+  int has_reload = (recog_memoized (insn) == CODE_FOR_loop_end_withreload);
 
   emit_insn_before (gen_addsi3 (loop->iter_reg, loop->iter_reg, constm1_rtx),
 		    loop->loop_end);
@@ -8202,6 +8203,15 @@ hwloop_fail (hwloop_info loop)
 
   JUMP_LABEL (insn) = loop->start_label;
   LABEL_NUSES (loop->start_label)++;
+
+  /* Copy back the counter in the memory as reload has to be handled
+     by ourself */
+  if (has_reload)
+    {
+      rtx mem_dest = SET_DEST (XVECEXP (PATTERN (loop->loop_end), 0, 2));
+      emit_insn (gen_movsi (mem_dest, loop->iter_reg));
+    }
+
   delete_insn (loop->loop_end);
 }
 
@@ -8214,7 +8224,9 @@ hwloop_pattern_reg (rtx insn)
 {
   rtx reg;
 
-  if (!JUMP_P (insn) || recog_memoized (insn) != CODE_FOR_loop_end)
+  if (!JUMP_P (insn)
+      || (recog_memoized (insn) != CODE_FOR_loop_end
+	  && recog_memoized (insn) != CODE_FOR_loop_end_withreload))
     return NULL_RTX;
 
   reg = SET_DEST (XVECEXP (PATTERN (insn), 0, 1));
