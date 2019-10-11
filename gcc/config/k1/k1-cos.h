8e9181087db3cd0756b5d7c3e2a88d2a032be0d0
jhascoet <jhascoet@kalray.eu>
2019-10-11 16:45:50 +0200

    Add mppa_rsrc by default for ClusterOS since it is a dependency ref T10802
    	gcc/config/k1/
    	* k1-cos.h (LIB_SPEC): Add mppa_rsrc library for ClusterOS
    	since it is now a dependency.
    
    Differential Revision: D2051

--- gcc/config/k1/k1-cos.h
+++ gcc/config/k1/k1-cos.h
@@ -38,7 +38,7 @@
  */
 #undef LIB_SPEC
 #define LIB_SPEC                                                               \
-  "--start-group -lmppacos -lc -lgloss --end-group "                           \
+  "--start-group -lmppacos -lmppa_rsrc -lc -lgloss --end-group "               \
   "%{!nostartfiles:%{!nodefaultlibs:%{!nostdlib:%{!T*:-Tmppacos.ld}}}}"
 
 #undef LINK_SPEC
