d719acf3c0bda9e0fd7d82519ffb12fe6bb6f44d
Marc Poulhiès <marc.poulhies@kalray.eu>
2019-12-17 11:35:59 +0100

    Remove Andey and Bostan specific tests.
    Remove obsolete tests.
    
    	gcc/
    	* testsuite/gcc.target/k1/builtin-fnarrowh.c: Removed.
    	* testsuite/gcc.target/k1/builtin-fnarrowhwp.c: Removed.
    	* testsuite/gcc.target/k1/builtin-fwidenb.c: Removed.
    	* testsuite/gcc.target/k1/builtin-fwidenbwp.c: Removed.
    	* testsuite/gcc.target/k1/builtin-fwidentwp.c: Removed.
    	* testsuite/gcc.target/k1/builtin-lhpz.c: Removed.
    	* testsuite/gcc.target/k1/builtin-lhpzn.c: Removed.
    	* testsuite/gcc.target/k1/builtin-lhpzu.c: Removed.
    	* testsuite/gcc.target/k1/builtin-lhpzun.c: Removed.
    	* testsuite/gcc.target/k1/builtin_float_conv.c: Removed.
    	* testsuite/gcc.target/k1/builtins_test.c: Removed.
    	* testsuite/gcc.target/k1/bwluwp_andey.c: Removed.
    	* testsuite/gcc.target/k1/bwluwp_bostan.c: Removed.
    	* testsuite/gcc.target/k1/float_builtins.c: Removed.
    	* testsuite/gcc.target/k1/builtin-cbs.c: Removed.
    	* testsuite/gcc.target/k1/builtin-cbsdl.c: Removed.
    	* testsuite/gcc.target/k1/builtin-ctzdl.c: Removed.
    	* testsuite/gcc.target/k1/compd.c: Removed.
    	* testsuite/gcc.target/k1/compdl.c: Removed.
    	* testsuite/gcc.target/k1/div2mul.c: Removed.
    	* testsuite/gcc.target/k1/movdf_bostan.c: Removed.
    	* testsuite/gcc.target/k1/movqi.c: Removed.
    	* testsuite/gcc.target/k1/muluuwd.c: Removed.
    	* testsuite/gcc.target/k1/overflow.c: Removed.
    	* testsuite/gcc.target/k1/popcount.c: Removed.
    	* testsuite/gcc.target/k1/pr_9337_segv.c: Removed.
    	* testsuite/gcc.target/k1/sbfx.c: Removed.
    	* testsuite/gcc.target/k1/sbfxd_andey.c: Removed.
    	* testsuite/gcc.target/k1/sbfxd_bostan.c: Removed.
    	* testsuite/gcc.target/k1/shifts_andey.c: Removed.
    	* testsuite/gcc.target/k1/shifts_bostan.c: Removed.
    	* testsuite/gcc.target/k1/srs_srsd_andey.c: Removed.
    	* testsuite/gcc.target/k1/srs_srsd_bostan.c: Removed.
    
    Differential Revision: D2973

--- gcc/testsuite/gcc.target/k1/builtin-fwidenb.c
+++
@@ -1,8 +0,0 @@
-/* { dg-do compile } */
-/* { dg-options "-O2" } */
-/* { dg-final { scan-assembler "fwidenlhw \\\$r\[0-9\]+ = \\\$r\[0-9\]+" } } */
-/* { dg-skip-if "Test valid only on Andey/Bostan" { *-*-* }  {  "*" } { "-march=k1a" "-march=k1b" } } */
-
-float f_widenb(unsigned int packed_half_float){
-  return __builtin_k1_fwidenlhw(packed_half_float);
-}
